9ae821add1e7124b859954eca379f6acb819b966
unknown <fredericogarcez13@gmail.com>
2023-08-01 19:16:05 -0300

    Modificação Js e Css

--- projeto/util.js
+++ projeto/util.js
@@ -32,7 +32,10 @@ function exibirTarefa(){
     list.innerHTML = ''
     for(let i = 0; i < values.length; i++)
     {
-        list.innerHTML += `<li><span>${values[i]['name']}</span><div class="botao"><button id='btnOk' onclick='checkTarefa("${values[i]['name']}", this)'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button><button id='btnClose' onclick='removeTarefa("${values[i]['name']}", this)'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/></svg></button></div></li>`
+        list.innerHTML += `<li><span>${values[i]['name']}</span><div class="botao">
+        <button class='btnOk' onclick='checkTarefa("${values[i]['name']}", this)' tittle="Marcar como concluido">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button>
+        </li>`
     }
 }
 
@@ -54,4 +57,28 @@ function ValidarInput(){
     let exists = values.find(x => x.name == inputValue);
     return !exists ? false : true 
 }
+
+function checkTarefa(tarefa, botao) {
+    botao.style.border = '1.5px solid black'
+    // Alterar o conteúdo do botão
+    botao.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/></svg>';
+    
+    // Alterar a cor do botão
+    botao.style.backgroundColor = "red"; // Aqui você pode definir a cor desejada
+
+    botao.classList.remove("btnOk");
+    botao.classList.add("btnClose");
+
+    botao.addEventListener("click", function(){
+        removeTarefa(tarefa);
+    })
+    
+    // Encontrar o elemento pai (a <li>) do botão
+    var liElement = botao.parentNode;
+    
+    let li = liElement.parentNode
+    // Alterar a cor do background da <li>
+    li.style.backgroundColor = "red"; // Aqui você pode definir a cor desejada
+  }
+
 exibirTarefa();
